8d4e2657d7947f6795df9a46267c9ab15dc40c65
Perry Defayette <defayetteperry@gmail.com>
2021-01-14 02:29:29 +0000

    Completed refactor.

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,7 +31,87 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present and backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is vertical", function() {
+    const result = wordSearch([
+      ['A', 'S', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'H', 'W', 'B', 'H', 'Z', 'D'],
+      ['Y', 'I', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'N', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'F', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'E', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'L', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is vertical and backwards", function() {
+    const result = wordSearch([
+      ['A', 'C', 'F', 'Q', 'U', 'D', 'A', 'L'],
+      ['S', 'H', 'W', 'B', 'H', 'L', 'Z', 'D'],
+      ['Y', 'C', 'F', 'Q', 'U', 'E', 'A', 'L'],
+      ['H', 'J', 'T', 'E', 'V', 'F', 'R', 'G'],
+      ['W', 'C', 'S', 'Y', 'E', 'N', 'R', 'L'],
+      ['B', 'R', 'E', 'N', 'E', 'I', 'Y', 'B'],
+      ['U', 'T', 'W', 'A', 'P', 'E', 'A', 'I'],
+      ['O', 'C', 'A', 'K', 'U', 'S', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is diagonal and backwards", function() {
+    const result = wordSearch([
+      ['D', 'C', 'F', 'Q', 'U', 'P', 'A', 'L'],
+      ['S', 'L', 'W', 'B', 'H', 'D', 'Z', 'D'],
+      ['Y', 'C', 'E', 'Q', 'U', 'X', 'A', 'L'],
+      ['H', 'J', 'T', 'F', 'V', 'L', 'R', 'G'],
+      ['W', 'C', 'S', 'Y', 'N', 'O', 'R', 'L'],
+      ['B', 'R', 'E', 'N', 'E', 'I', 'Y', 'B'],
+      ['U', 'T', 'W', 'A', 'P', 'V', 'E', 'I'],
+      ['O', 'C', 'A', 'K', 'U', 'S', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is diagonal", function() {
+    const result = wordSearch([
+      ['D', 'C', 'F', 'Q', 'U', 'P', 'A', 'L'],
+      ['S', 'L', 'W', 'B', 'H', 'D', 'Z', 'D'],
+      ['Y', 'C', 'E', 'Q', 'U', 'X', 'L', 'L'],
+      ['H', 'J', 'T', 'F', 'V', 'E', 'R', 'G'],
+      ['W', 'C', 'S', 'Y', 'F', 'O', 'R', 'L'],
+      ['B', 'R', 'E', 'N', 'E', 'I', 'Y', 'B'],
+      ['U', 'T', 'I', 'A', 'P', 'V', 'E', 'I'],
+      ['O', 'E', 'A', 'K', 'U', 'S', 'A', 'S'],
+      ['S', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,86 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  let visitedLetters = [];
+  const HEIGHT = letters.length;
+  const WIDTH = letters[0].length;
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  
+  // for horizontal check
+  for (let l of horizontalJoin) {
+    if (l.includes(word))
+      return true;
+    else if (l.split('').reverse().join('').includes(word))
+      return true;
+  }
+  
+  //for vertical check
+  let arr = [];
+  //loop each column
+  for (let column = 0; column < letters[0].length; column++) {
+    arr.push([]);
+    //loop each row
+    for (let row = 0; row < letters.length; row++) {
+      //create array with letters being added using row/column indices
+      arr[column].push(letters[row][column]);
     }
-}
+    
+    //check if array contains word after all rows have been added
+    if (arr[column].join('').includes(word)) {
+      return true;
+    } else if (arr[column].reverse().join('').includes(word)) {
+      return true;
+    }
+  }
 
-module.exports = wordSearch
+  /*
+       VVV DIAGONAL CHECK VVV
+  */
+
+  //recursive function that goes to edge of array and records letters
+  const checkCells = function(cell, direction) {
+    let x = cell.x;
+    let y = cell.y;
+    
+    //check bounds of array
+    if (x < 0 || y < 0)
+      return false;
+    //here too
+    if (x > WIDTH || y > HEIGHT)
+      return false;
+    else {
+      //if within bounds, push to array and call recursively updated to new direction
+      visitedLetters.push(letters[x][y]);
+      checkCells({x: x + direction.upDown, y: y + direction.leftRight}, direction);
+    }
+  };
+
+  //direction list
+  const DIRS = [
+    {x:  1, y:  1},
+    {x:  1, y: -1},
+    {x: -1, y:  1},
+    {x: -1, y: -1},
+  ];
+  
+  //loop through direction list
+  for (let i = 0; i < DIRS.length; i++) {
+    //loop over each row for first column and last column
+    for (let row = 0; row < HEIGHT; row++) {
+      let column = WIDTH - 1;
+      if (i < 2)
+        column = 0;
+      
+      //start checkCells recursive function from loop indices.
+      checkCells({x: row, y: column}, {leftRight: DIRS[i].x, upDown: DIRS[i].y});
+
+      //check if checkCells' vector caught our word, if not reset and loop
+      if (visitedLetters.join('').includes(word)) {
+        //console.log(visitedLetters);
+        return true;
+      }
+      visitedLetters = [];
+    }
+  }
+  return false;
+};
+module.exports = wordSearch;
